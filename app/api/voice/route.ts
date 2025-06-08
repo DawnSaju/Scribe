c9a4444b53bc117536f81496fec56e3421251e5c
DawnSaju <70035674+DawnSaju@users.noreply.github.com>
2025-06-09 02:00:36 +0400

    added a voiceAPI for better chat experience

---
+++ app/api/voice/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request){
+    try {
+        const body = await request.json();
+        const text = body.text;
+
+        const endpoint = process.env.VOICE_ENDPOINT;
+        const key = process.env.KEY;
+
+        if (!endpoint) {
+            return NextResponse.json({ error: "Error endpoint not set", status: 500});
+        } 
+
+        const data = await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${key}`,
+            },
+            body: JSON.stringify({
+                "model": "elevenlabs",
+                "input": text,
+                "voice": "Will (US male)"
+            })
+        });
+
+        if (!data.ok) {
+            const error = await data.text();
+            return NextResponse.json({ error }, { status: data.status });
+        }
+
+        const audio = await data.arrayBuffer();
+
+        return new NextResponse(audio, {
+            headers: {
+                "Content-Type": 'audio/wav',
+                "Content-Length": audio.byteLength.toString(),
+            }
+        });
+    } catch (error) {
+        console.log("Error in VoiceAPI", error);
+        return NextResponse.json({ error: "Error in voiceAPI", status: 500})
+    }
+}
